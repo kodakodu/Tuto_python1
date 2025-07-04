7e8d0e33980bafd42c5b574bce789037ef20cb99
kodakodu <kodakodu@gmail.com>
2025-07-04 15:00:21 +0200

    30:27

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#Importing the components we need
 from PySide6.QtWidgets import QApplication,QWidget
 
+#sys
 import sys
 app = QApplication(sys.argv)
 
 window = QWidget()
 window.show()
 
+#Start the event loop
 app.exec()
