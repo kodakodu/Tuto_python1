85e9696842406f626e8e68604efa3cd4d16c225c
kodakodu <kodakodu@gmail.com>
2025-07-07 10:50:02 +0200

    35:28

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-#Importing the components we need
-from PySide6.QtWidgets import QApplication,QWidget
-
 #sys
 import sys
+
+#Importing the components we need
+from PySide6.QtWidgets import QApplication,QMainWindow, QPushButton
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button Holder app")
+        button = QPushButton("Press Me!")
+
+        self.setCentralWidget(button)
+
+
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = ButtonHolder()
 window.show()
 
 #Start the event loop
