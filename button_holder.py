93fa344c4ed7437fe4e63b70effdce11e3ca6121
kodakodu <kodakodu@gmail.com>
2025-07-07 12:05:42 +0200

    40:41

--- button_holder.py
+++ button_holder.py
@@ -4,6 +4,8 @@ class ButtonHolder(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Button Holder app")
+        
         button = QPushButton("Press Me!")
+        button.setFixedSize(100,60)
 
         self.setCentralWidget(button)
